df3ce2936f72318cfe30073e11585946cff0ccc5
isc-lperezra <lperezra@intersystems.com>
2023-04-19 14:28:40 +0200

    Minor updates

--- install/installer.sh
+++ install/installer.sh
@@ -1,6 +1,13 @@
 nohup $IRISSYS/ISCAgentUser start &>/dev/null &
 sleep 1
 iris session $ISC_PACKAGE_INSTANCENAME -U %SYS << END
+do ##class(%SYSTEM.Process).CurrentDirectory("$PWD")
+$@
+if '\$Get(sc, 1) do ##class(%SYSTEM.Process).Terminate(, 1)
+zn "%SYS"
+do ##class(SYS.Container).QuiesceForBundling()
+do ##class(Security.Users).UnExpireUserPasswords("*")
+
 do \$SYSTEM.OBJ.Load("/management/PhonebookInstaller.xml", "ck") 
 do ##class(Phonebook.Installer).RunInstall()
 halt
